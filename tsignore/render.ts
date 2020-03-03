614e5619180a3b12827db1b3b776a26b4eda36c2
winton <mail@wintoni.us>
2020-03-03 15:22:45 -0800

    Generator

---
+++ tsignore/render.ts
@@ -0,0 +1,164 @@
+import { LoadedEvent } from "./loaded"
+import patch from "./patch"
+import ssr from "./ssr"
+
+export class Render {
+  browser: boolean = null
+  patch: typeof patch = null
+  ssr: typeof ssr = null
+
+  doc: Document
+  events: Record<string, boolean>
+
+  htmlProps = {
+    className: true,
+    id: true,
+    innerHTML: true,
+    nodeValue: true,
+    tabIndex: true,
+    textContent: true,
+    value: true,
+  }
+
+  constructor() {
+    this.reset()
+  }
+
+  reset(): void {
+    this.events = {}
+  }
+
+  loaded(): void {
+    if (this.ssr) {
+      this.doc = this.ssr.dom.document
+    } else {
+      this.doc = document
+    }
+
+    if (!this.browser && !global["window"]) {
+      global["window"] = {}
+    }
+
+    window["jsxFactory"] = this.createElement.bind(this)
+  }
+
+  loadedBy(event: LoadedEvent): void {
+    const { by } = event
+
+    if (by.element) {
+      this.patch.create(
+        by,
+        "element",
+        {
+          element: by.element.bind(by),
+          order: -1,
+        },
+        {
+          afterElement: this.afterElement.bind(this),
+          args: [event],
+          order: 0,
+        }
+      )
+    }
+  }
+
+  afterElement(event: LoadedEvent): Element {
+    const { by } = event
+    const { memo } = this.patch.find(by.element)
+    const element = memo.element as Element
+
+    if (element && by.cache !== element) {
+      by.cache = element
+
+      if (by.id) {
+        const docEl = this.doc.getElementById(by.id)
+
+        if (docEl) {
+          docEl.parentNode.replaceChild(element, docEl)
+        }
+      }
+    }
+
+    return by.cache
+  }
+
+  createElement(tagName): Element {
+    const node =
+      tagName.nodeType === 1
+        ? tagName
+        : this.doc.createElement(tagName)
+
+    for (let i = 1; i < arguments.length; ++i) {
+      // eslint-disable-next-line
+      const arg = arguments[i]
+      if (!arg) {
+        continue
+      }
+      if (!arg.constructor || arg.constructor === Object) {
+        for (
+          let j = 0, ks = Object.keys(arg);
+          j < ks.length;
+          ++j
+        ) {
+          const key = ks[j],
+            val = arg[key]
+          if (val === undefined) {
+            continue
+          }
+          if (key === "style") {
+            node.style.cssText = val
+          } else if (
+            typeof val !== "string" ||
+            this.htmlProps[key]
+          ) {
+            node[key] = val
+            //set synthetic events for onUpperCaseName
+            if (
+              key[0] === "o" &&
+              key[1] === "n" &&
+              key.charCodeAt(2) < 91 &&
+              key.charCodeAt(2) > 64 &&
+              !this.events[key]
+            ) {
+              this.doc.addEventListener(
+                key.slice(2).toLowerCase(),
+                function(e): any {
+                  let tgt: any = e.target
+                  do {
+                    if (tgt[key]) {
+                      return tgt[key](e)
+                    }
+                  } while ((tgt = tgt.parentNode))
+                }
+              )
+              this.events[key] = true
+            }
+          } else {
+            node.setAttribute(key, val)
+          }
+        }
+      } else {
+        if (Array.isArray(arg)) {
+          for (let k = 0; k < arg.length; ++k) {
+            if (arg[k]) {
+              node.appendChild(
+                arg[k].nodeType
+                  ? arg[k]
+                  : this.doc.createTextNode(arg[k])
+              )
+            }
+          }
+        } else {
+          node.appendChild(
+            arg.nodeType
+              ? arg
+              : this.doc.createTextNode(arg)
+          )
+        }
+      }
+    }
+    return node
+  }
+}
+
+export default new Render()
