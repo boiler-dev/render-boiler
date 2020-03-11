0d2150bc5a72c3b41143bfc0d771cb844e0e7450
winton <mail@wintoni.us>
2020-03-10 17:53:30 -0700

    Upgrade action syntax

--- boiler.ts
+++ boiler.ts
@@ -1,4 +1,3 @@
-import { join } from "path"
 import { ActionBoiler } from "boiler-dev"
 
 export const install: ActionBoiler = async () => {
@@ -13,31 +12,20 @@ export const install: ActionBoiler = async () => {
   return actions
 }
 
-export const generate: ActionBoiler = async ({
-  cwdPath,
-  files,
-}) => {
+export const generate: ActionBoiler = async () => {
   const actions = []
 
-  for (const file of files) {
-    const { name, source } = file
+  actions.push({
+    action: "write",
+    path: "src/render.ts",
+    sourcePath: "tsignore/render.ts",
+  })
 
-    if (name === "render.ts") {
-      actions.push({
-        action: "write",
-        path: join(cwdPath, "src", name),
-        source,
-      })
-    }
-
-    if (name === "render.spec.tsx") {
-      actions.push({
-        action: "write",
-        path: join(cwdPath, "test", name),
-        source,
-      })
-    }
-  }
+  actions.push({
+    action: "write",
+    path: "test/render.spec.tsx",
+    sourcePath: "tsignore/render.spec.tsx",
+  })
 
   return actions
 }
